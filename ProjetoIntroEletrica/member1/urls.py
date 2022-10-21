ce7568cf3fb80eab4485fc3390902878ab1f3cba
mate-us12 <113392846+mate-us12@users.noreply.github.com>
2022-10-21 19:26:06 -0300

    Add files via upload

--- ProjetoIntroEletrica/member1/urls.py
+++ ProjetoIntroEletrica/member1/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('index.html', views.index, name='indexi.html'),
+    path('', views.index, name='index.html'),
     path('Area-do-paciente.html', views.paciente, name='Area-do-paciente.html'),
 ]
--- ProjetoIntroEletrica/member1/views.py
+++ ProjetoIntroEletrica/member1/views.py
@@ -3,8 +3,9 @@ from django.http import HttpResponse
 from django.template import loader
 
 def index(request):
-    template = loader.get_template('Menu-inicial.html')
-    return HttpResponse(template.render())
+#    template = loader.get_template('Menu-inicial.html')
+#    return HttpResponse(template.render())
+    return render(request, 'Menu-inicial.html', {})
 
 def paciente(request):
     return render(request, 'Area-do-paciente.html', {})
