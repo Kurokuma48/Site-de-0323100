52ded2a8493c9a6a897f3236e5f1161b87b9d6de
mate-us12 <113392846+mate-us12@users.noreply.github.com>
2022-10-21 17:47:01 -0300

    Add files via upload

--- ProjetoIntroEletrica/member1/views.py
+++ ProjetoIntroEletrica/member1/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render
+#from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 def index(request):
-    return HttpResponse("Hello World!")
+    template = loader.get_template('Menu-inicial.html')
+    return HttpResponse(template.render())
+
+def paciente(request):
+    template = loader.get_template('Area-do-paciente.html')
+    return HttpResponse(template.render())
