8199184a47439ec184cbc8df02034549bf62cf1d
mate-us12 <113392846+mate-us12@users.noreply.github.com>
2022-10-21 22:05:08 -0300

    Add files via upload

--- ProjetoIntroEletrica/member1/models.py
+++ ProjetoIntroEletrica/member1/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Ocorrencia(models.Model):
+    local = models.TextField()
+    telefone = models.IntegerField()
+    CPF = models.IntegerField(unique=True)
+    nome = models.CharField(max_length=255)
+    ocorrencia = models.TextField()
--- ProjetoIntroEletrica/member1/urls.py
+++ ProjetoIntroEletrica/member1/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('index.html', views.index, name='indexi.html'),
+    path('index.html', views.index, name='index.html'),
     path('', views.index, name='index.html'),
     path('Area-do-paciente.html', views.paciente, name='Area-do-paciente.html'),
 ]
