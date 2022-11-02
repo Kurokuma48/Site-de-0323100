b08f809f7165cc88bb0fa1d569ec961cf187daad
mate-us12 <113392846+mate-us12@users.noreply.github.com>
2022-11-01 22:24:56 -0300

    Add files via upload

--- C++/teste.cpp
+++ C++/teste.cpp
@@ -43,74 +43,108 @@ int main(){
 
 //   teste.adiconarRua(1, 2, 5.0);
 
-	 Rede.adicionarRua(1 , ,  )
-	 Rede.adicionarRua(1 , ,  )
-	 Rede.adicionarRua(2 , ,  )
-	 Rede.adicionarRua(2 , ,  )
-	 Rede.adicionarRua(3 , ,  )
-	 Rede.adicionarRua(3 , ,  )
-	 Rede.adicionarRua(4 , ,  )
-	 Rede.adicionarRua(4 , ,  )
-	 Rede.adicionarRua(5 , ,  )
-	 Rede.adicionarRua(5 , ,  )
-	 Rede.adicionarRua(6 , ,  )
-	 Rede.adicionarRua(6 , ,  )
-	 Rede.adicionarRua(7 , ,  )
-	 Rede.adicionarRua(7 , ,  )
-	 Rede.adicionarRua(8 , ,  )
-	 Rede.adicionarRua(8 , ,  )
-	 Rede.adicionarRua(9 , ,  )
-	 Rede.adicionarRua(9 , ,  )
-	 Rede.adicionarRua(10 , ,  )
-	 Rede.adicionarRua(10 , ,  )
-	 Rede.adicionarRua(11 , ,  )
-	 Rede.adicionarRua(11 , ,  )
-	 Rede.adicionarRua(12 , ,  )
-	 Rede.adicionarRua(12 , ,  )
-	 Rede.adicionarRua(13 , ,  )
-	 Rede.adicionarRua(13 , ,  )
-	 Rede.adicionarRua(14 , ,  )
-	 Rede.adicionarRua(14 , ,  )
-	 Rede.adicionarRua(15 , ,  )
-	 Rede.adicionarRua(15 , ,  )
-	 Rede.adicionarRua(16 , ,  )
-	 Rede.adicionarRua(16 , ,  )
-	 Rede.adicionarRua(17 , ,  )
-	 Rede.adicionarRua(17 , ,  )
-	 Rede.adicionarRua(18 , ,  )
-	 Rede.adicionarRua(18 , ,  )
-	 Rede.adicionarRua(19 , ,  )
-	 Rede.adicionarRua(19 , ,  )
-	 Rede.adicionarRua(20 , ,  )
-	 Rede.adicionarRua(20 , ,  )
-	 Rede.adicionarRua(21 , ,  )
-	 Rede.adicionarRua(21 , ,  )
-	 Rede.adicionarRua(22 , ,  )
-	 Rede.adicionarRua(22 , ,  )
-	 Rede.adicionarRua(23 , ,  )
-	 Rede.adicionarRua(23 , ,  )
-	 Rede.adicionarRua(24 , ,  )
-	 Rede.adicionarRua(24 , ,  )
-	 Rede.adicionarRua(25 , ,  )
-	 Rede.adicionarRua(25 , ,  )
-	 Rede.adicionarRua(26 , ,  )
-	 Rede.adicionarRua(26 , ,  )
-	 Rede.adicionarRua(27 , ,  )
-	 Rede.adicionarRua(27 , ,  )
-	 Rede.adicionarRua(28 , ,  )
-	 Rede.adicionarRua(28 , ,  )
-	 Rede.adicionarRua(29 , ,  )
-	 Rede.adicionarRua(29 , ,  )
-	 Rede.adicionarRua(30 , ,  )
-	 Rede.adicionarRua(30 , ,  )
-	 Rede.adicionarRua(31 , ,  )
-	 Rede.adicionarRua(31 , ,  )
-	 Rede.adicionarRua(32 , ,  )
-	 Rede.adicionarRua(32 , ,  )
-	 Rede.adicionarRua(33 , ,  )
-	 Rede.adicionarRua(33 , ,  )
-	 Rede.adicionarRua(34 , ,  )
-	 Rede.adicionarRua(34 , ,  )
+	Rede.adicionarRua(1, 2, )
+	Rede.adicionarRua(2, 1, )
+
+	Rede.adicionarRua(2, 3, )
+	Rede.adicionarRua(3, 2, )
+
+	Rede.adicionarRua(3, 4, )
+	Rede.adicionarRua(4, 3, )
+
+	Rede.adicionarRua(4, 5, )
+	Rede.adicionarRua(5, 4, )
+
+	Rede.adicionarRua(5, 6, )
+	Rede.adicionarRua(6, 5, )
+
+	Rede.adicionarRua(6, 7, )
+	Rede.adicionarRua(7, 6, )
+
+	Rede.adicionarRua(7, 8, )
+	Rede.adicionarRua(8, 7, )
+
+	Rede.adicionarRua(8, 9, )
+	Rede.adicionarRua(9, 8, )
+
+	Rede.adicionarRua(9, 10, )
+	Rede.adicionarRua(10, 9, )
+
+	Rede.adicionarRua(10, 11, )
+	Rede.adicionarRua(11, 10, )
+
+	Rede.adicionarRua(11, 12, )
+	Rede.adicionarRua(12, 11, )
+
+	Rede.adicionarRua(12, 13, )
+	Rede.adicionarRua(13, 12, )
+
+	Rede.adicionarRua(13, 14, )
+	Rede.adicionarRua(14, 13, )
+
+	Rede.adicionarRua(14, 15, )
+	Rede.adicionarRua(15, 14, )
+
+	Rede.adicionarRua(15, 16, )
+	Rede.adicionarRua(16, 15, )
+
+	Rede.adicionarRua(16, 17, )
+	Rede.adicionarRua(17, 16, )
+
+	Rede.adicionarRua(17, 18, )
+	Rede.adicionarRua(18, 17, )
+
+	Rede.adicionarRua(18, 19, )
+	Rede.adicionarRua(19, 18, )
+
+	Rede.adicionarRua(19, 20, )
+	Rede.adicionarRua(20, 19, )
+
+	Rede.adicionarRua(20, 21, )
+	Rede.adicionarRua(21, 20, )
+
+	Rede.adicionarRua(21, 22, )
+	Rede.adicionarRua(22, 21, )
+
+	Rede.adicionarRua(22, 23, )
+	Rede.adicionarRua(23, 22, )
+
+	Rede.adicionarRua(23, 24, )
+	Rede.adicionarRua(24, 23, )
+
+	Rede.adicionarRua(24, 25, )
+	Rede.adicionarRua(25, 24, )
+
+	Rede.adicionarRua(25, 26, )
+	Rede.adicionarRua(26, 25, )
+
+	Rede.adicionarRua(26, 27, )
+	Rede.adicionarRua(27, 26, )
+
+	Rede.adicionarRua(27, 28, )
+	Rede.adicionarRua(28, 27, )
+
+	Rede.adicionarRua(28, 29, )
+	Rede.adicionarRua(29, 28, )
+
+	Rede.adicionarRua(29, 30, )
+	Rede.adicionarRua(30, 29, )
+
+	Rede.adicionarRua(30, 31, )
+	Rede.adicionarRua(31, 30, )
+
+	Rede.adicionarRua(31, 32, )
+	Rede.adicionarRua(32, 31, )
+
+	Rede.adicionarRua(32, 33, )
+	Rede.adicionarRua(33, 32, )
+
+	Rede.adicionarRua(33, 34, )
+	Rede.adicionarRua(34, 33, )
+
+	Rede.adicionarRua(34, 1, )
+	Rede.adicionarRua(1, 34, )
+
 
 
 
