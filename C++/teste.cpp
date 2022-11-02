2c99c47f3af4ef6025b466317ba5772beda75323
mate-us12 <113392846+mate-us12@users.noreply.github.com>
2022-11-01 22:15:56 -0300

    Add files via upload

--- C++/teste.cpp
+++ C++/teste.cpp
@@ -6,42 +6,115 @@ using namespace std;
 int main(){
     Rede teste;
     try{
-        teste.adicionarSemaforo(1, 0.0, 0.0);
-        teste.adicionarSemaforo(2, 0.0, 0.0);
-        teste.adicionarSemaforo(3, 0.0, 0.0);
-        teste.adicionarSemaforo(4, 0.0, 0.0);
-        teste.adicionarSemaforo(5, 0.0, 0.0);
-        teste.adicionarSemaforo(6, 0.0, 0.0);
-        teste.adicionarSemaforo(7, 0.0, 0.0);
-        teste.adicionarSemaforo(8, 0.0, 0.0);
-        teste.adicionarSemaforo(9, 0.0, 0.0);
-        teste.adicionarSemaforo(10, 0.0, 0.0);
-        teste.adiconarRua(1, 2, 5.0);
-        teste.adiconarRua(1, 4, 10.0);
-        teste.adiconarRua(2, 10, 3.0);
-        teste.adiconarRua(2, 5, 1.0);
-        teste.adiconarRua(2, 1, 4.0);
-        teste.adiconarRua(3, 4, 7.0);
-        teste.adiconarRua(3, 10, 10.0);
-        teste.adiconarRua(4, 2, 12.0);
-        teste.adiconarRua(4, 1, 9.0);
-        teste.adiconarRua(5, 6, 5.0);
-        teste.adiconarRua(5, 7, 7.0);
-        teste.adiconarRua(6, 7, 8.0);
-        teste.adiconarRua(6, 4, 2.0);
-        teste.adiconarRua(6, 9, 2.0);
-        teste.adiconarRua(6, 10, 2.0);
-        teste.adiconarRua(7, 2, 7.0);
-        teste.adiconarRua(7, 8, 2.0);
-        teste.adiconarRua(7, 5, 5.0);
-        teste.adiconarRua(8, 6, 3.0);
-        teste.adiconarRua(8, 7, 1.0);
-        teste.adiconarRua(8, 9, 2.0);
-        teste.adiconarRua(9, 10, 4.0);
-        teste.adiconarRua(10, 2, 3.0);
-        teste.adiconarRua(10, 6, 3.0);
+	 Rede.adicionarSemaforo(1 , 0.0, 0.0)
+	 Rede.adicionarSemaforo(2 , 0.0, 0.0)
+	 Rede.adicionarSemaforo(3 , 0.0, 0.0)
+	 Rede.adicionarSemaforo(4 , 0.0, 0.0)
+	 Rede.adicionarSemaforo(5 , 0.0, 0.0)
+	 Rede.adicionarSemaforo(6 , 0.0, 0.0)
+	 Rede.adicionarSemaforo(7 , 0.0, 0.0)
+	 Rede.adicionarSemaforo(8 , 0.0, 0.0)
+	 Rede.adicionarSemaforo(9 , 0.0, 0.0)
+	 Rede.adicionarSemaforo(10 , 0.0, 0.0)
+	 Rede.adicionarSemaforo(11 , 0.0, 0.0)
+	 Rede.adicionarSemaforo(12 , 0.0, 0.0)
+	 Rede.adicionarSemaforo(13 , 0.0, 0.0)
+	 Rede.adicionarSemaforo(14 , 0.0, 0.0)
+	 Rede.adicionarSemaforo(15 , 0.0, 0.0)
+	 Rede.adicionarSemaforo(16 , 0.0, 0.0)
+	 Rede.adicionarSemaforo(17 , 0.0, 0.0)
+	 Rede.adicionarSemaforo(18 , 0.0, 0.0)
+	 Rede.adicionarSemaforo(19 , 0.0, 0.0)
+	 Rede.adicionarSemaforo(20 , 0.0, 0.0)
+	 Rede.adicionarSemaforo(21 , 0.0, 0.0)
+	 Rede.adicionarSemaforo(22 , 0.0, 0.0)
+	 Rede.adicionarSemaforo(23 , 0.0, 0.0)
+	 Rede.adicionarSemaforo(24 , 0.0, 0.0)
+	 Rede.adicionarSemaforo(25 , 0.0, 0.0)
+	 Rede.adicionarSemaforo(26 , 0.0, 0.0)
+	 Rede.adicionarSemaforo(27 , 0.0, 0.0)
+	 Rede.adicionarSemaforo(28 , 0.0, 0.0)
+	 Rede.adicionarSemaforo(29 , 0.0, 0.0)
+	 Rede.adicionarSemaforo(30 , 0.0, 0.0)
+	 Rede.adicionarSemaforo(31 , 0.0, 0.0)
+	 Rede.adicionarSemaforo(32 , 0.0, 0.0)
+	 Rede.adicionarSemaforo(33 , 0.0, 0.0)
+	 Rede.adicionarSemaforo(34 , 0.0, 0.0)
 
-        teste.getMenorCaminho(4, 6);
+//   teste.adiconarRua(1, 2, 5.0);
+
+	 Rede.adicionarRua(1 , ,  )
+	 Rede.adicionarRua(1 , ,  )
+	 Rede.adicionarRua(2 , ,  )
+	 Rede.adicionarRua(2 , ,  )
+	 Rede.adicionarRua(3 , ,  )
+	 Rede.adicionarRua(3 , ,  )
+	 Rede.adicionarRua(4 , ,  )
+	 Rede.adicionarRua(4 , ,  )
+	 Rede.adicionarRua(5 , ,  )
+	 Rede.adicionarRua(5 , ,  )
+	 Rede.adicionarRua(6 , ,  )
+	 Rede.adicionarRua(6 , ,  )
+	 Rede.adicionarRua(7 , ,  )
+	 Rede.adicionarRua(7 , ,  )
+	 Rede.adicionarRua(8 , ,  )
+	 Rede.adicionarRua(8 , ,  )
+	 Rede.adicionarRua(9 , ,  )
+	 Rede.adicionarRua(9 , ,  )
+	 Rede.adicionarRua(10 , ,  )
+	 Rede.adicionarRua(10 , ,  )
+	 Rede.adicionarRua(11 , ,  )
+	 Rede.adicionarRua(11 , ,  )
+	 Rede.adicionarRua(12 , ,  )
+	 Rede.adicionarRua(12 , ,  )
+	 Rede.adicionarRua(13 , ,  )
+	 Rede.adicionarRua(13 , ,  )
+	 Rede.adicionarRua(14 , ,  )
+	 Rede.adicionarRua(14 , ,  )
+	 Rede.adicionarRua(15 , ,  )
+	 Rede.adicionarRua(15 , ,  )
+	 Rede.adicionarRua(16 , ,  )
+	 Rede.adicionarRua(16 , ,  )
+	 Rede.adicionarRua(17 , ,  )
+	 Rede.adicionarRua(17 , ,  )
+	 Rede.adicionarRua(18 , ,  )
+	 Rede.adicionarRua(18 , ,  )
+	 Rede.adicionarRua(19 , ,  )
+	 Rede.adicionarRua(19 , ,  )
+	 Rede.adicionarRua(20 , ,  )
+	 Rede.adicionarRua(20 , ,  )
+	 Rede.adicionarRua(21 , ,  )
+	 Rede.adicionarRua(21 , ,  )
+	 Rede.adicionarRua(22 , ,  )
+	 Rede.adicionarRua(22 , ,  )
+	 Rede.adicionarRua(23 , ,  )
+	 Rede.adicionarRua(23 , ,  )
+	 Rede.adicionarRua(24 , ,  )
+	 Rede.adicionarRua(24 , ,  )
+	 Rede.adicionarRua(25 , ,  )
+	 Rede.adicionarRua(25 , ,  )
+	 Rede.adicionarRua(26 , ,  )
+	 Rede.adicionarRua(26 , ,  )
+	 Rede.adicionarRua(27 , ,  )
+	 Rede.adicionarRua(27 , ,  )
+	 Rede.adicionarRua(28 , ,  )
+	 Rede.adicionarRua(28 , ,  )
+	 Rede.adicionarRua(29 , ,  )
+	 Rede.adicionarRua(29 , ,  )
+	 Rede.adicionarRua(30 , ,  )
+	 Rede.adicionarRua(30 , ,  )
+	 Rede.adicionarRua(31 , ,  )
+	 Rede.adicionarRua(31 , ,  )
+	 Rede.adicionarRua(32 , ,  )
+	 Rede.adicionarRua(32 , ,  )
+	 Rede.adicionarRua(33 , ,  )
+	 Rede.adicionarRua(33 , ,  )
+	 Rede.adicionarRua(34 , ,  )
+	 Rede.adicionarRua(34 , ,  )
+
+
+
+        teste.getMenorCaminho(3, 7);
     }
     catch(invalid_argument* e){
         cout << e->what() << endl;
